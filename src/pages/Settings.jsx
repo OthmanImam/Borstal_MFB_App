b07f4c3283b6803556cac44256c7c8f09a26f533
OthmanImam <imkargi@gmail.com>
2024-10-16 06:14:41 -0700

    update commit

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -160,7 +160,7 @@ function Settings() {
           <div className="mt-8 flex">
             <button
               type="submit"
-              className="rounded-md border-green-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              className="rounded-md border-green-900 px-3 py-2 text-sm font-semibold bg-green-900 text-white shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
               Save
             </button>
